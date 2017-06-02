dbf6a5296690a0441074cd518cf81748d3f9e036
BeginReactNative <nguyenphuctrungkien@gmail.com>
2017-06-02 16:29:44 +0700

    Add New Files

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+    state = {  }
+    render() {
+        return (
+            <View>
+                <Text>
+                    AAAA
+                </Text>
+            </View>
+        );
+    }
+}
+
+export default App;
